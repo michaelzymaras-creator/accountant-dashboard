9dcbf3fa14a050ead93c3642d8de56928daca059
unknown <Administrator@OROFOS.tsalas.local>
2026-03-17 12:39:10 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,11 @@ console.log("USER:", user)
 
 async function addClient(){
 
+if(!user){
+alert("Δεν έχει γίνει login ακόμα")
+return
+}
+
 if(!name) return alert("Βάλε όνομα")
 
 const { data, error } = await supabase
@@ -27,12 +32,13 @@ const { data, error } = await supabase
 user_id: user.id,
 name,
 afm,
-monthly_fee: Number(fee)
+monthly_fee: fee,
 payment_status: "pending"
 }])
 .select()
 
 if(error){
+console.log(error)
 alert(error.message)
 return
 }
